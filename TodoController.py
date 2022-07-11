3983f58229bca69ab0c68309ddfc5ed3d879ae47
Szymon440 <szymonss@gmail.com>
2022-07-11 14:01:30 +0200

    Todo-01 code refactor

--- TodoController.py
+++ TodoController.py
@@ -1,20 +1,21 @@
 from flask import render_template, redirect, url_for
 from TodoModel import TodoModel
 
+
 class TodoController(object):
-    def home(Todo):
-        return render_template("index.html" , todoList=TodoModel.getAllTodo(Todo))
+    def home(todo):
+        return render_template("index.html", todoList=TodoModel.getAllTodo(todo))
 
-    def add(Todo, db):
-        TodoModel.InsertDataToDatabase(Todo,db)
+    def add(todo, db):
+        TodoModel.InsertDataToDatabase(todo, db)
         return redirect(url_for("home"))
 
-    def update(Todo , db, id):
-        todo = TodoModel.getTodoById(Todo, id)
-        TodoModel.updateTodoStatus(todo , db)
+    def update(todo, db, id):
+        todoTakenById = TodoModel.getTodoById(todo, id)
+        TodoModel.updateTodoStatus(todoTakenById, db)
         return redirect(url_for("home"))
 
-    def delete(Todo , db, id):
-        todo = TodoModel.getTodoById(Todo, id)
-        TodoModel.deleteTodoById(todo , db)
+    def delete(todo, db, id):
+        todoTakenById = TodoModel.getTodoById(todo, id)
+        TodoModel.deleteTodoById(todoTakenById, db)
         return redirect(url_for("home"))
